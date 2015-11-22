1b894b810574097bfee7c1091522facbb20d6b95
Satoyama Tomoaki <tsatoyama@vitalify.jp>
2015-11-22 11:45:03 +0900

    インデント調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
 
-def show
-  # パラメータに該当するデータをDBから検索する
-  # コーディング上、モデル名は大文字始まりでインスタンス変数は小文字始まり
-  @item = Item.find(params[:id])
-end
+  def show
+    # パラメータに該当するデータをDBから検索する
+    # コーディング上、モデル名は大文字始まりでインスタンス変数は小文字始まり
+    @item = Item.find(params[:id])
+  end
 
 end
